91ad11727d7c7bff6cd21d663d40b00245454b01
Jeb <jedsboss21@yahoo.com>
2021-05-08 02:43:56 -0700

    Delete index.js

--- instructions/index.js
+++
@@ -1,8 +0,0 @@
-const {meta} = require('./meta');
-
-const indexBy = (array, prop) => array.reduce((output, item) => {
-  output[item[prop]] = item;
-  return output;
-}, {});
-
-module.exports = indexBy(meta, 'instruction');
